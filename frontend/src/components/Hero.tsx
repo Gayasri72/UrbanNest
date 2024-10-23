eea377fb0d080f62f5044eba04a19030b3fd3add
Gayasri72 <gayasripethum1@gmail.com>
2024-10-23 23:27:54 +0530

    updates the header

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -1,15 +1,39 @@
+import { useState, useEffect } from "react";
+
 const Hero = () => {
-    return (
-      <div className="bg-sky-500 pb-16">
-        <div className="container mx-auto flex flex-col gap-2">
-          <h1 className="text-5xl text-white font-bold">
-              Find your next stay
-          </h1>
-          <p className="text-2xl text-white">
-              Search low prices on houses for your dream life...
-          </p>
-        </div>
-      </div>
-    );
+  const reactDescriptions = [
+    "Find your next stay",
+    "Discover your perfect getaway",
+    "Book your ideal accommodation",
+  ];
+
+  const [currentDescription, setCurrentDescription] = useState(
+    reactDescriptions[0]
+  );
+
+  function genRandomInt(max) {
+    return Math.floor(Math.random() * (max + 1));
   }
-  export default Hero;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentDescription(reactDescriptions[genRandomInt(2)]);
+    }, 10000); // 30 seconds
+
+   
+    return () => clearInterval(interval);
+  }, [reactDescriptions]);
+
+  return (
+    <div className="bg-sky-500 pb-16">
+      <div className="container mx-auto flex flex-col gap-2">
+        <h1 className="text-5xl text-white font-bold">{currentDescription}</h1>
+        <p className="text-2xl text-white">
+          Search low prices on houses for your dream life...
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
